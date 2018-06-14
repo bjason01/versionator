241a9af52079b8a6dbedb7c5eb7bd70b249ed74d
Brandon Valle <bjvtamayo78@gmail.com>
2018-06-14 13:47:47 -0400

    Make gem to write the new version for a specific project

--- lib/versionator/file.rb
+++ lib/versionator/file.rb
@@ -2,6 +2,7 @@ module Versionator
   module File
     def write(version="0.0.0")
       ::File.open(Versionator.version_file, 'w') {|f| f.write(version) }
+      ::File.write('app/views/layouts/dashboard/_version.html.erb', version)
     end
     
     def read
